ba384574409bb094cc431fd70e34b9835412b1b7
Omar Branez <omar.branez@gmail.com>
2021-03-01 15:57:53 -0500

    commit tree 0ea74e2c9e0bfabb5c45dc4c5a032129596062a2

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    attr_reader :string_url
+
+    def initialize(string_url)
+        @string_url = string_url
+    end
+
+    def get_response_body #seems consistent so far for GET requests and responses
+        uri = URI.parse(string_url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        data = JSON.parse(self.get_response_body)
+    end
+    #in this case, @parse is purely for open-uri and for json
+end    
